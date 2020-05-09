64fdba4c237ffe9cef053f0e3da5360156ee4858
Alex Griep <alexgriep@hotmail.com>
2020-05-09 18:46:50 -0500

    added components for books and addbooks

--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+//allow cross orgin requests
+app.use(cors());
 
 const uri = "mongodb+srv://alexgriep:test123@gql-griep-q2kq3.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
